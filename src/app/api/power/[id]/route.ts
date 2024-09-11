a690010a5ef2bf03055f60f7f3aed951e7b0b474
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-09-11 14:34:06 -0400

    add request PUT power

--- src/app/api/power/[id]/route.ts
+++ src/app/api/power/[id]/route.ts
@@ -7,8 +7,34 @@ export async function GET(req: NextRequest) {
         const power = await prisma.power.findUnique({
             where: { id }
         });
+
+        if (!power)
+            return NextResponse.json({ success: false, message: 'power not found' }, { status: 404 })
+
         return NextResponse.json({ success: true, data: power });
     } catch (error) {
         return NextResponse.json({ success: false, message: error });
+    }
+}
+
+export async function PUT(req: NextRequest) {
+    try {
+        const data = await req.json();
+        if (data.id)
+            return NextResponse.json({ success: false, message: 'Cannot modify id' }, { status: 500 })
+        const id = parseInt(req.url.split('/power/')[1])
+        const power = await prisma.power.update({
+            where: { id },
+            data: {
+                name: data.name,
+                dangerous: data.dangerous
+            }
+        });
+        if (!power)
+            return NextResponse.json({ success: false, message: 'power not found' }, { status: 404 })
+
+        return NextResponse.json({ success: true, data: power });
+    } catch (error) {
+        return NextResponse.json({ success: false, message: 'Error updating power' });
     }
 }
