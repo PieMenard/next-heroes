a008843a5f5c1a4cb96f83e29e4ff031b3ba1b54
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-16 00:28:36 -0400

    implement hero search

--- src/app/components/SearchBox.tsx
+++ src/app/components/SearchBox.tsx
@@ -1,10 +1,34 @@
-const SearchBox = () => {
+import { FormEvent } from 'react';
+
+type SearchProps = {
+  query: string;
+  setQuery: (query: string) => void;
+  handleSearch: (query: string) => void;
+};
+
+const SearchBox = ({ query, setQuery, handleSearch }: SearchProps) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleSearch(query);
+  };
+
   return (
     <div>
-      <input
-        className="border-2 px-2 rounded-sm bg-slate-100"
-        placeholder="Search a hero..."
-      ></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="border-2 px-2 rounded-sm bg-slate-100 mr-2"
+          placeholder="Search a hero..."
+          value={query}
+          type="text"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="rounded-md px-2 bg-slate-500 text-white"
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,40 @@ import HeroCard from './components/HeroCard';
 
 export default function Home() {
   const [heroes, setHeroes] = useState<Hero[]>([]);
+  const [filteredheroes, setFilteredHeroes] = useState<Hero[]>([]);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const getHeroes = async () => {
       const response = await fetch('/api/hero');
       const results = await response.json();
       setHeroes(results.data.results);
+      setFilteredHeroes(results.data.results);
     };
     getHeroes();
   }, []);
+
+  const handleSearch = async (query: string) => {
+    const search = heroes.filter((hero) =>
+      hero.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredHeroes(search);
+  };
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="font-semibold text-xl text-center my-4">Heroes</h1>
-      <SearchBox />
+      <SearchBox
+        query={query}
+        setQuery={setQuery}
+        handleSearch={handleSearch}
+      />
       <ul className="flex flex-col items-center">
-        {heroes.map((hero) => (
-          <HeroCard hero={hero} />
+        {filteredheroes.length === 0 && (
+          <div className="my-4">No heroes found :(</div>
+        )}
+        {filteredheroes.map((hero) => (
+          <HeroCard key={hero.id} hero={hero} />
         ))}
       </ul>
     </div>
